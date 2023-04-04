49532dfe85c6c94d59cf164e64aa1a658da2ddee
DanniD33 <danni.denmark7@gmail.com>
2023-04-03 20:34:40 -0400

    fixed bugs by wrapping functions in promises. Video is rendering to canvas but now I need to draw the points.

--- image/webcam/webcam.js
+++ image/webcam/webcam.js
@@ -1,13 +1,17 @@
+// const { platform } = require("os");
+
 const video = document.getElementById('webcam');
 const canvas = document.querySelector('canvas');
 const liveView = document.getElementById('liveView');
 const demosSection = document.getElementById('demos');
 const enableWebcamButton = document.getElementById('webcamButton');
+const ctx = canvas.getContext('2d');
 
 //LSTM layer: Best way to describe it is like  a  mini  recording of a neural network; We are able to look back  at prior detections of hand (flattened array of frames(x,)) to  make a prediction  
 
-
-
+// async function platform(){
+//   await tf.setBackend('cpu');
+// };
 
 /*
 1.) First Check to see if we are able to get webcam using getUserMedia 
@@ -27,6 +31,9 @@ function getUserMediaSupported() {
 } else {
   console.log('getUserMedia() is not supported by your browser');
 }
+
+
+
 //In this code block (25), we,'ll draw the  stream from   our webcam to the canvas
 const width = 640;
 const height = 480;
@@ -53,26 +60,38 @@ video.onplay = function() {
 
 //Once we hear the event, our function will load tensor api , we will began the stream of our webcam then need to break our stream into frames that we can then pass into our predictWebcam   
 
-let model;
+// let model = poseDetection.SupportedModels.BlazePose;
 let children = [];
 
-async function enableCam(event) {
+//create a function that Loads Model (cannot use await outside of async)
+async function model(){
+await tf.loadLayersModel('https://cdn.jsdelivr.net/npm/@tensorflow-models/pose-detection');
+// model.summar#y();
+}
 
+async function enableCam(event) {
+// await model;
   // cocoSsd.load().then(function (loadedModel){
   //   model = loadedModel;
-  //   demosSection.classList.remove('invisible');
+  async function detector(){
+    model = poseDetection.SupportedModels.BlazePose;
+  await poseDetection.createDetector(model, {runtime:'tfjs', solutionPath: 'https://cdn.jsdelivr.net/npm/@mediapipe/pose'}).json();
+  // return await detector.json();
+}
+    demosSection.classList.remove('invisible');
   // });
 
 
+  // let model = poseDetection.SupportedModels.BlazePose;
+
+// async function poses(){
+//   const estimationConfig = {enableSmoothing: true};
+//  await detector.estimatePoses(canvas, estimationConfig);
+// }
+
+
+  event.target.classList.add('removed'); 
   
-const detector = await poseDetection.createDetector(poseDetection.SupportedModels.BlazePose, {runtime:'tfjs', solutionPath: 'https://cdn.jsdelivr.net/npm/@mediapipe/pose'});
-
-const poses = await detector.estimatePoses(canvas);
-
-
-  // event.target.classList.add('removed'); 
-  
-
 
   // Load stream from webcam using getUserMedia api
   const constraints = {
@@ -83,14 +102,12 @@ const poses = await detector.estimatePoses(canvas);
   };
     // video.videoWidth = 640;
     // video.videoHeight = 480;
-  
+  // add canvas style April 3, 2023 8:03pm
+    // canvas.style.zIndex = 
   await navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
     video.srcObject = stream;
+    video.addEventListener('loadeddata', predictWebcam);
   });
-  
-
-  video.addEventListener('loadeddata', predictWebcam);
-
 }
 
 
@@ -101,9 +118,13 @@ const poses = await detector.estimatePoses(canvas);
     //What does classify mean? --> 
       //the current frame is passed through a prediction function that returns whether the current frame's score (.score is a property on a model in tensor) is oover 66%
 
-    function predictWebcam() {
+    async function predictWebcam(model) {
+      
       // Now let's start classifying a frame in the stream.
-      model.detect(canvas).then(function (predictions) {
+      async function classify(){
+
+      await model.predict(canvas)
+      .then(function (predictions) {
       // model.detect(canvas.captureStream()).then(function (predictions) {
         // Remove any highlighting we did previous frame.
         for (let i = 0; i < children.length; i++) {
@@ -113,7 +134,7 @@ const poses = await detector.estimatePoses(canvas);
       //remove frame from DOM and from Memory after its classified; We can still reference a copy of the frame but its otherwise deleted as long as we have a parent node (aka liveView), we can draw from 
 
         children.splice(0); //copy of the rest of the frames that haven't been classified
-
+    
         // Now lets loop through predictions and draw them to the live view if
         // they have a high confidence score.
         for (let n = 0; n < predictions.length; n++) {
@@ -148,12 +169,12 @@ const poses = await detector.estimatePoses(canvas);
             children.push(p);
           }
         }
-        
+      
         // Call this function again to keep predicting when the browser is ready.
         window.requestAnimationFrame(predictWebcam);
       });
     }
-    
+  }
     
 
 //we need objetDetection
